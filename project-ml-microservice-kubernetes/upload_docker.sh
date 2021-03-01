a42605aa237ebf4bd7bb174746644978f95fa9f4
William Geuns <william.geuns@gmail.com>
2021-03-01 18:53:27 +0100

    running docker automatically, uploading docker image, starting a kubernetes pod from the docker image

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=williamgeuns/udacity-ml-project
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag mlk8s_app:latest williamgeuns/udacity-ml-project
 
 # Step 3:
 # Push image to a docker repository
+docker push williamgeuns/udacity-ml-project
+
